a9cb4f69e213899b8ea4f46abeec5c268793e862
Ruslan <shaggy-axel@mail.ru>
2023-02-16 12:00:55 +0100

    add ci config (#7)
    * add ci config
    
    * map port for postgres
    
    * add redis
    
    * fix tests: open files in test

--- src/apps/deals/conftest.py
+++ src/apps/deals/conftest.py
@@ -4,24 +4,24 @@ import pytest
 from django.conf import settings
 
 
-@pytest.fixture(scope="session")
-def deals_csv() -> IO:
-    return open(settings.TEST_FILES["deals_csv"])
+@pytest.fixture()
+def deals_csv() -> str:
+    return settings.TEST_FILES["deals_csv"]
 
 
-@pytest.fixture(scope="session")
-def deals_csv_second() -> IO:
-    return open(settings.TEST_FILES["deals_csv_second"])
+@pytest.fixture()
+def deals_csv_second() -> str:
+    return settings.TEST_FILES["deals_csv_second"]
 
 
-@pytest.fixture(scope="session")
-def deals_csv_invalid_file_extension() -> IO:
-    return open(settings.TEST_FILES["deals_csv_invalid_file_extension"])
+@pytest.fixture()
+def deals_csv_invalid_file_extension() -> str:
+    return settings.TEST_FILES["deals_csv_invalid_file_extension"]
 
 
-@pytest.fixture(scope="session")
-def deals_txt_invalid_data() -> IO:
-    return open(settings.TEST_FILES["deals_txt_invalid_data"])
+@pytest.fixture()
+def deals_txt_invalid_data() -> str:
+    return settings.TEST_FILES["deals_txt_invalid_data"]
 
 
 @pytest.fixture
@@ -31,13 +31,17 @@ def expect_fail_invalid_data():
             "total": ["A valid number is required."],
             "quantity": ["A valid integer is required."],
             "date": [
-                "Datetime has wrong format. Use one of these formats instead: YYYY-MM-DDThh:mm[:ss[.uuuuuu]][+HH:MM|-HH:MM|Z]."
+                "Datetime has wrong format. "
+                "Use one of these formats instead: "
+                "YYYY-MM-DDThh:mm[:ss[.uuuuuu]][+HH:MM|-HH:MM|Z]."
             ],
             "line": "2",
         },
         {
             "date": [
-                "Datetime has wrong format. Use one of these formats instead: YYYY-MM-DDThh:mm[:ss[.uuuuuu]][+HH:MM|-HH:MM|Z]."
+                "Datetime has wrong format. "
+                "Use one of these formats instead: "
+                "YYYY-MM-DDThh:mm[:ss[.uuuuuu]][+HH:MM|-HH:MM|Z]."
             ],
             "line": "5",
         },
